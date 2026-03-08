e5e09e1652121c1de52e6d5b37332dba870bd360
Bayu Ramadhan <bayuramadhan0401@gmail.com>
2026-03-09 03:08:25 +0700

    lightrays

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-
+import LightRays from "../components/LightRays";
 
 export default function Home() {
   return (
-    <div>
-      <p>home</p>
+    <div style={{ width: "100%", height: "100vh", position: "relative" }}>
+      <LightRays />
+
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          color: "white",
+          fontSize: "40px",
+        }}
+      >
+        My Portfolio
+      </div>
     </div>
   );
 }
